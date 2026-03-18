ffd78a91391495aa3dbdae6d140d13ac4913b774
Travis Van Couvering <tvanc@rentecdirect.com>
2026-03-18 14:48:12 -0700

    Revert "Auto-rename visibility callback PR0Callback to PR8Callback"
    This reverts commit 10ee42d7cd648828a903bf212ef088e58cb25904.

--- classes/Feature/WhatsNew/Visibility/PR0Callback.php
+++ classes/Feature/WhatsNew/Visibility/PR0Callback.php
@@ -7,10 +7,7 @@ use Rentec\BackOffice\Feature\WhatsNew\Visibility\Callback;
  * @todo Delete this file before merging the PR that introduces it
  * @noinspection PhpUnused
  */
-/**
- * @see https://github.com/RentecTravis/workflow-test/pull/8
- */
-final readonly class PR8Callback implements Callback {
+final readonly class PR0Callback implements Callback {
     public function getDescription(): string
     {
         return 'Hide announcement on mobile';
